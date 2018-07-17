c47642e02b969f73404e1b3d9c41f4e4296daa1a
chijinxin <chijinxind@hotmail.com>
2018-07-17 16:34:10 +0800

    add Adapter

--- src/适配器模式/ClassAdapter.cpp
+++ src/适配器模式/ClassAdapter.cpp
@@ -37,9 +37,56 @@ class Adapter : public USBer, public PS2    //继承USBer 实现PS2接口
     }
 };
 
+
+/*
+ * 例子2
+ * 角色：target、Adaptee、Adapter
+ */
+//目标接口类，客户需要的接口
+class Target{
+public:
+    Target(){}
+    virtual ~Target(){}
+
+    virtual void Request()   //定义标准接口
+    {
+        cout<<"Target::Request()"<<endl;
+    }
+};
+
+//需要适配的类
+class Adaptee{
+public:
+    Adaptee(){}
+    ~Adaptee(){}
+
+    void SpecificRequest()
+    {
+        cout<<"Adaptee::SpecificRequest()"<<endl;
+    }
+};
+
+//类模式，适配器类，通过public继承获得接口继承的效果，通过priviate继承获得实现继承的效果
+class Adapter2 : public Target, private Adaptee
+{
+public:
+    Adapter2(){}
+    ~Adapter2(){}
+    virtual void Request()   //实现target顶一下的Request接口
+    {
+        cout<<"Adapter2::Request()"<<endl;
+        this->SpecificRequest();
+        cout<<"-------------------"<<endl;
+    }
+};
+
 int main()
 {
     PS2* p = new Adapter();
     p->isPS2();
+
+    //类模式Adapter
+    Target* pTarget = new Adapter2();
+    pTarget->Request();
     return 0;
 }
--- src/适配器模式/ObjectAdapter.cpp
+++ src/适配器模式/ObjectAdapter.cpp
@@ -1,5 +1,93 @@
-
 //
 // Created by chijinxin on 18-7-17.
 //
+#include <iostream>
+using namespace std;
+/*
+ * 对象适配器
+ * 1. 以PS2转USB转化器（适配器）为例
+ */
+//PS2接口
+class PS2{
+public:
+    virtual ~PS2(){}
+    virtual void isPS2() = 0;
+};
+//USB接口
+class USB{
+public:
+    virtual void idUSB() = 0 ;
+};
+//USB实现
+class USBer : public USB {
+public:
+    void idUSB() override {
+        cout<<"This is a USB"<<endl;
+    }
+};
 
+//适配器
+class Adapter : public PS2{
+private:
+    USB* usb;
+
+public:
+    Adapter(USB* usb)
+    {
+        this->usb = usb;
+    }
+
+    void isPS2() override {
+        usb->idUSB();
+    }
+};
+
+/*
+ * 例2 对象适配器模式
+ * 角色：Target Adaptee Adapter
+ * 客户端需要调用Request方法，而Adaptee类中没有该方法，为了使客户端能够使用Adaptee类
+ * 需要提供一个包装（Wrapper）类Adapter，这个包装类包装了一个Adaptee的实例，从而将客户端
+ * 与Adaptee衔接了起来。由于Adapter与Adaptee是委派关系
+ */
+//Target接口
+class Target{
+public:
+    virtual void Request()=0;
+};
+
+//Adaptee类
+class Adaptee{
+public:
+    void SpecificRequest()
+    {
+        cout<<"Called SpecificRequest()"<<endl;
+    }
+};
+
+//Adapter类
+class Adapter2 : public Target {
+private:
+    Adaptee* adaptee_;
+public:
+    Adapter2(Adaptee* a)
+    {
+        this->adaptee_ = a;
+    }
+
+    void Request() override {
+        adaptee_->SpecificRequest();
+    }
+};
+
+
+int main()
+{
+    USB* usb = new USBer();
+    PS2* p = new Adapter(usb);
+    p->isPS2();
+
+    Adaptee* aa = new Adaptee();
+    Target *t = new Adapter2(aa);
+    t->Request();
+    return 0;
+}
